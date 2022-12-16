5b4ff52bd4178111e37e78665327b5145a4dcc3f
Kirill <yudanovkr@yandex.ru>
2022-12-16 11:04:12 +0300

    Task2 complet

--- praktikum-1/Task2/Program.cs
+++ praktikum-1/Task2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3  */
+using static System.Console;
+
+Write("Enter num a: ");
+int a = int.Parse(ReadLine()!);
+
+Write("Enter num b: ");
+int b = int.Parse(ReadLine()!);
+
+if (a > b)
+    {
+        WriteLine($"max = {a}");
+    }
+    else 
+    {
+        WriteLine($"max = {b}");
+    }
