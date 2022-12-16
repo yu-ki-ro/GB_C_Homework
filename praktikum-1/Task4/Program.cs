a7d2c6bc14384471e80b5dfcb27af2ac2a5d80ce
Kirill <yudanovkr@yandex.ru>
2022-12-16 11:23:34 +0300

    Task4 complet

---
+++ praktikum-1/Task4/Program.cs
@@ -0,0 +1,39 @@
+﻿/*Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+using static System.Console;
+
+Clear();
+
+WriteLine("Enter num a: ");
+int a = int.Parse(ReadLine());
+
+WriteLine("Enter num b: ");
+int b = int.Parse(ReadLine());
+
+WriteLine("Enter num c: ");
+int c = int.Parse(ReadLine());
+
+if (a > b)
+    {
+        if (a > c)
+            {
+                WriteLine($"max = {a}");
+            }
+        else 
+            {
+                WriteLine($"max = {c}");
+            }
+    }
+    else if (b > c)
+            {
+                WriteLine($"max = {b}");
+            }
+        else
+            {
+                WriteLine($"max = {c}");
+            }    
