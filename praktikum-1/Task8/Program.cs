fda90beb575c817d6d6f0229184203cdde533ed5
Kirill <yudanovkr@yandex.ru>
2022-12-16 11:47:01 +0300

    Task8 complet

---
+++ praktikum-1/Task8/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+using static System.Console;
+Clear();
+
+WriteLine("Enter N: ");
+float N = float.Parse(ReadLine()!);
+int i = 1;
+while (i <= N)
+    if (i % 2 == 1)
+        i++;
+    else 
+    {
+        Write($"{i} ");
+        i++;
+    }
+WriteLine();
