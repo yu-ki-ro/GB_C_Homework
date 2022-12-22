11b9d76f26d4274d854f37c823081d6ba3c8ae29
Kirill <yudanovkr@yandex.ru>
2022-12-22 20:38:53 +0300

    task13 complete

---
+++ praktikum-2/Task13/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 6
+
+78 -> третьей цифры нет
+
+32679 -> 6 */
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите число");
+int num = int.Parse(ReadLine()!);
+int i = 10;
+
+if (num < 100)
+    {
+        WriteLine("Третей цифры нет");
+        return;
+    }
+    else 
+    {
+        while (( num / i > 0 ) && (num / ( i * 10 ) != 0 ))
+        {
+            i = i * 10;
+        }
+    }
+num = num / (i/100);
+num = num % 10;
+
+
+
+WriteLine(num);
