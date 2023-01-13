a5108674b900e5b7f691eaf70ca166f43a43b55a
Kirill <yudanovkr@yandex.ru>
2023-01-13 11:47:52 +0300

    task 25 complete

---
+++ praktikum-4/task25/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+Clear();
+
+Write("Input number: ");
+int number = int.Parse(ReadLine()!);
+Write("Input stepen: ");
+int step = int.Parse(ReadLine()!);
+int sum=GetStep(number,step);
+WriteLine($"A in step B = {sum}");
+
+
+
+
+int GetStep(int A,int B)
+{
+    int result = 1;
+    while (B>0)
+    {
+        result *= A;
+        B--;
+    }
+    return result;
+}
