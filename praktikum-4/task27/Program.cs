be51b46d709f5fee4cf1c591c1e77ad51ade56c6
Kirill <yudanovkr@yandex.ru>
2023-01-13 14:02:29 +0300

    task29 complete

---
+++ praktikum-4/task27/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 27: Напишите программу, реализующую метод, который принимает на вход число и выдаёт сумму цифр в числе.
+
+using static System.Console;
+Clear();
+
+Write("Input number: ");
+int number = int.Parse(ReadLine()!);
+
+int sum=GetSum(number);
+WriteLine($"SUM  = {sum}");
+
+
+
+
+
+int GetSum(int A)
+{
+    int i=0;
+    while (A != 0)
+    {
+        i = i + A % 10;
+        A = A / 10;
+    }
+    return i;
+}
