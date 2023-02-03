391ad0b54ccc215b900800b62da90fba5aa42792
Kirill <yudanovkr@yandex.ru>
2023-02-03 08:15:41 +0300

    task 64 complete

---
+++ praktikum-9/task64/Program.cs
@@ -0,0 +1,18 @@
+﻿//  Напишите программу, которая реализует рекурсивный метод вывода всех натуральных числел в промежутке от N до 1.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Clear();
+
+int j = 8;
+double Fatorial (int n)
+{
+    if (n==1) return 1;
+    else  return n * Fatorial(n-1);
+}
+for (int i = j; i != 0; i--)
+{
+    Console.Write($"{i}!: ");
+    Console.WriteLine(Fatorial(i));
+}
